faa134fa4f2ffa4559c4414828632bdb6b7bbefc
DanielRebolledo-dev <drebolledo@indemin.cl>
2025-09-01 17:11:20 -0400

    functions

--- src/bases/05-functions.ts
+++ src/bases/05-functions.ts
@@ -4,12 +4,48 @@ function greet(name: string): string{
 }
 
 // Funcion flecha
-const greetFlecha = (name: string): string =>{
-    return `Hello, ${name}`;
-}
+const greetFlecha = (name: string) => `Hello, ${name}`;
+
 
 const message = greet('Daniel');
 const messageFlecha = greetFlecha('Daniel');
 
 console.log(message);
 console.log(messageFlecha);
+
+interface User {
+    uid: string,
+    username: string,
+}
+
+function getUser(): User{
+    return {
+        uid: 'ABC123',
+        username: 'El_Papi1502'
+    }
+}
+const getUser2 = () =>({
+    uid: 'ABC123',
+    username: 'El_Papi1502'
+})
+
+const user = getUser();
+const user2 = getUser2();
+console.log(user);
+console.log(user2);
+
+const myNumbers: number[] = [1,2,3,4,5,6];
+
+myNumbers.forEach( function(value){
+    console.log({value})
+})
+
+myNumbers.forEach((value) =>{
+    console.log({value})
+});
+
+myNumbers.forEach(console.log);
+
+myNumbers.forEach((value, index, arr) =>{
+    console.log(value,index,arr);
+})
